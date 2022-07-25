761535e46ad6565cba519c0d674598cdcb70c993
Tobias Fischer <info@tobiasfischer.info>
2022-07-26 09:33:44 +1000

    Remove hack as it's upstreamed
    Co-authored-by: Mark Harfouche <mark.harfouche@gmail.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -21,15 +21,6 @@ else
   export TORCH_CUDA_ARCH_LIST="${TORCH_CUDA_ARCH_LIST}+PTX"
   export FORCE_CUDA=1
   export CC="$GCC"
-
-  # 2022/07 hmaarrfk
-  # Big Giant Hack for Pytorch 1.12.0
-  # They limited the version in
-  # https://github.com/pytorch/pytorch/blob/2ddb722bc6b166e9b32f316400e89437fe6b1ac3/torch/utils/cpp_extension.py#L51-L57=
-  # But I've already patched 10.0 for compatibility with the CUDA
-  # versions we support
-  # Removing these lines makes their compilation check fail and emit a warning instead
-  sed -i '/ (MINIMUM_GCC_VERSION,/d' ${SP_DIR}/torch/utils/cpp_extension.py
 fi
 
 ${PYTHON} -m pip install . -vv
