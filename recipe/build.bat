672f528521ad7395f759004c172d004506ee5f50
H. Vetinari <h.vetinari@gmx.com>
2026-01-25 23:41:38 +1100

    set `/permissive-` on windows

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,8 +1,12 @@
+@echo on
+
 if not "%cuda_compiler_version%" == "None" (
     set TORCH_CUDA_ARCH_LIST=5.0;6.0;7.0;7.5;8.0;8.6;8.9;9.0;10.0;12.0+PTX
     set FORCE_CUDA=1
 ) else (
     set FORCE_CUDA=0
 )
+
+set "CXXFLAGS=%CXXFLAGS% /permissive-"
 
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
